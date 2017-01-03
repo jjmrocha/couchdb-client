e249ef646a243e51749eeb41b06c0a7b266c2e25
Joaquim Rocha <joaquim.rocha@timwe.com>
2017-01-03 18:10:06 +0000

    Copyright update

--- src/main/java/net/uiqui/couchdb/api/DB.java
+++ src/main/java/net/uiqui/couchdb/api/DB.java
@@ -2,7 +2,7 @@
 a * CouchDB-client
  * ==============
  * 
- * Copyright (C) 2016 Joaquim Rocha <jrocha@gmailbox.org>
+ * Copyright (C) 2016-17 Joaquim Rocha <jrocha@gmailbox.org>
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
